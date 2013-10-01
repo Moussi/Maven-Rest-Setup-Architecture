d1ab1ae55e9f3d7e42b494001aa4c391efaed7dc
MoussImagin <moussimagin@moussiPowerShare.(none)>
2013-10-01 02:31:11 +0100

    Setup Architecture

---
+++ com.powershare.core/com.powershare.dal/src/main/java/com/powershare/dal/App.java
@@ -0,0 +1,13 @@
+package com.powershare.dal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
